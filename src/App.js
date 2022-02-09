e22add84e45b8d4284abab2b218bc0b42a6dcc45
Agostina Tomalino <agostomalino@gmail.com>
2022-02-08 23:50:23 -0300

    Se agrega el componente ItemCount como hijo del ItemListContainer para agregar o restar items

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import NavBar from './components/menu/NavBar';
 import  Footer from './components/Footer';
 import ItemListContainer from './components/menu/ItemListContainer';
 
+
 function App() {
   return (
     <main>
       <NavBar textoDeLogo={"Kumo dibujos"}/> {/* si paso como valor un boolean si o si debe estar entre brackets */}
-      <ItemListContainer greeting={"Bienvenido al mundo Kumo!"}/>
+      <ItemListContainer greeting={"Camisa tiger"}/>
       <Footer/>
     </main>  
     
--- src/components/menu/ItemListContainer.jsx
+++ src/components/menu/ItemListContainer.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import ItemCount from '../ItemCount';
 
 const ItemListContainer = ({greeting}) => {
   return <div>
       <h5>{greeting}</h5>
+      <ItemCount stock={5} initial={1}/>
   </div>;
 };
 
